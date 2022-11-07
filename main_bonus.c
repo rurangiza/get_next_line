b3c77b5ff89be559386760664dab55142a7ed7c8
Arsene Rurangiza <arurangi@mi-r1-p3.s19.be>
2022-11-07 15:25:43 +0100

    added bonus parts

--- main_bonus.c
+++ main_bonus.c
@@ -1,32 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 15:06:21 by arurangi          #+#    #+#             */
-/*   Updated: 2022/11/07 15:11:33 by arurangi         ###   ########.fr       */
+/*   Created: 2022/11/03 15:58:16 by Arsene            #+#    #+#             */
+/*   Updated: 2022/11/07 15:17:54 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
 #include <fcntl.h>
 
 int	main(void)
 {
-	int		fd;
-	char	*str;
+	int		fd_1;
+	int		fd_2;
+	int		fd_3;
+	int		index;
 
-	while (1)
+	fd_1 = open("file_1", O_RDONLY);
+	fd_2 = open("file_2", O_RDONLY);
+	fd_3 = open("file_3", O_RDONLY);
+	index = 0;
+	while (index < 4)
 	{
-		str = get_next_line(fd);
-		if (str == NULL)
-			break ;
-		printf("%s", str);
-		free(str);
+		printf("[1] %s\n", get_next_line(fd_1));
+		printf("[2] %s\n", get_next_line(fd_2));
+		printf("[3] %s\n", get_next_line(fd_3));
+		index++;
 	}
-	close(fd);
+	close(fd_1);
+	close(fd_2);
+	close(fd_3);
 	return (0);
 }
